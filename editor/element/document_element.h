54cb81dec9a9136e8067c05b1ad59b67ffc2f966
ankit <sahuankit.murhu@gmail.com>
2026-08-29 22:44:14 +0530

    feat: document element interface

---
+++ editor/element/document_element.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+
+class DocumentElement {
+public:
+  virtual ~DocumentElement() = default;
+  virtual void render() = 0;
+};
+
+class TextElement : public DocumentElement {
+private:
+  std::string text;
+
+public:
+  TextElement(const std::string &text);
+  void render() override;
+};
+
+class ImageElement : public DocumentElement {
+private:
+  std::string imagePath;
+
+public:
+  ImageElement(const std::string &path);
+  void render() override;
+};
