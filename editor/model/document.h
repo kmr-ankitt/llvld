db71e1b075b231efc18838d9e8393d9e4aee0c56
ankit <sahuankit.murhu@gmail.com>
2026-08-30 10:14:46 +0530

    feat: document model class defination

--- editor/model/document.h
+++ editor/model/document.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "../element/document_element.h"
+#include "vector"
+
+class DocumentModel {
+private:
+  std::vector<DocumentElement *> elements;
+
+public:
+  DocumentModel();
+
+  void addElement(DocumentElement *element);
+  std::vector<DocumentElement> getElements();
+};
