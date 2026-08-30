0b47d34ae365252be606b607883f3cbb42c26963
ankit <sahuankit.murhu@gmail.com>
2026-08-30 12:03:55 +0530

    chore: simplify vector return in getElements

--- editor/model/document.cpp
+++ editor/model/document.cpp
@@ -4,10 +4,6 @@ void DocumentModel::addElement(DocumentElement *element) {
   elements.push_back(element);
 }
 
-std::vector<DocumentElement> DocumentModel::getElements() {
-  std::vector<DocumentElement> elementCopies;
-  for (DocumentElement *element : elements) {
-    elementCopies.push_back(*element);
-  }
-  return elementCopies; 
+std::vector<DocumentElement*> DocumentModel::getElements() {
+  return elements; 
 }
