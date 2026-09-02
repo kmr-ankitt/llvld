54b0f4d9e6c1e725e0c54b39d8ac5c2fc7ad827e
ankit <sahuankit.murhu@gmail.com>
2026-09-02 23:24:23 +0530

    feat: restarant id auto increment

--- delivery/models/restaurant.h
+++ delivery/models/restaurant.h
@@ -1,5 +1,8 @@
 #pragma once
+
+#include "../interfaces/location.h"
 #include "menu_item.h"
+
 #include <string>
 #include <vector>
 
@@ -7,10 +10,18 @@ class Restaurant {
 private:
   static unsigned int restaurantId;
   std::string name;
+  Location location;
   std::vector<MenuItem *> menu;
 
 public:
-  Restaurant(const std::string &name) : name(name) { restaurantId++; };
+  Restaurant(const std::string &name, int latitude, int longitude,
+             std::string &loc)
+      : name(name) {
+    restaurantId++;
+    location.latitude = latitude;
+    location.longitude = longitude;
+    location.name = loc;
+  };
 
   // getters
   unsigned int getRestaurantId() const { return restaurantId; }
@@ -18,6 +29,7 @@ public:
   std::string getName() const { return name; }
 
   std::vector<MenuItem *> getMenu() const { return menu; }
+  Location getLocation() const { return location; }
 
   // setters
   void setName(const std::string &name) { this->name = name; };
